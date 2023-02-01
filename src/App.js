988c8fdb1766b3ee3d1b9ae0cc6dba219a22db3a
maximilian-r-alba <100059063+maximilian-r-alba@users.noreply.github.com>
2023-02-01 09:46:42 -0500

    Begin building simple user authentication with UseState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import './Components/Review.js';
+import {UseState , UseEffect} from "react";
+import {Routes, Route} from "react-router-dom";
 
 function App() {
+
+  const user = UseState({name: null, avatar_url: null, bio: null, loggedIn: false})
+
+  UseEffect (
+    fetch("http://localhost:9292/users")
+    .then(r => r.json())
+    .then(data => console.log(data))
+  ,[])
 
   fetch("http://localhost:9292/")
   .then((r) => r.json())
