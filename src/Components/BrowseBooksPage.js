c7f7e6f4dc30c970af0c7143b13541756fec7967
maximilian-r-alba <100059063+maximilian-r-alba@users.noreply.github.com>
2023-02-02 16:08:00 -0500

    Implement bookscard in Browsebookspage

--- src/Components/BrowseBooksPage.js
+++ src/Components/BrowseBooksPage.js
@@ -1,17 +1,34 @@
 import {useState, useEffect} from "react";
+import "./BooksCard.js";
 
 function BrowseBooksPage(){
 
     const [allBooks, setAllBooks] = useState([])
+    const [bookCards, setBookCards] = useState([])
     
+    function renderCards(books){
+        cards = books.map((book) => {
+            <BooksCard book = {book} />
+        })
+
+        setBookCards(cards)
+    }
+     
     useEffect(() => {
         fetch('http://localhost:9292/books')
             .then(r => r.json())
-            .then(data => setAllBooks(data))
+            .then(data => {
+                setAllBooks(data)
+                renderCards(data)
+            })
     }, [])
 
     return (
-        <h1>SHOW BOOKS HERE</h1>
+        <>
+            <h1>SHOW BOOKS HERE</h1>
+            {bookCards}
+        </>
+       
     )
 }
 
