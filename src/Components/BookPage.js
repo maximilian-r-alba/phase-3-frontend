02fe926b555e65eebd508b5d0833e80e52f5b643
maximilian-r-alba <100059063+maximilian-r-alba@users.noreply.github.com>
2023-02-07 20:42:51 +0000

    Begin building review card syntax

--- src/Components/BookPage.js
+++ src/Components/BookPage.js
@@ -1,16 +1,39 @@
 import {useState, useEffect} from "react";
+import ReviewCard from './ReviewCard'
 
+function BookPage({book}){
 
-function BookPage(){
+    useEffect(() => {
+        fetch(`http:localhost:9292/books/${id}`)
+        .then(r => r.json())
+        .then(data => console.log(data))
+    }, [])
 
+    useEffect(() => {
+        fetch(`http:localhost:9292/books/${id}/reviews`)
+        .then(r => r.json())
+        .then(data => console.log(data))
+    }, [])
    
+    function reviewsList(reviews){
+        reviewList = reviews.map((review) => <ReviewCard key={review.id} review={review} ></ReviewCard>)
+    }
+
     return (
         <>
-            <h1>SHOW BOOK HERE</h1>
-        
+            <h1>{book.title}</h1>
+            <img src={book.cover_url} alt="book cover image"></img>
+            <p>By: {book.author}</p>
+            <p>{book.subgenre}</p>
+            <p>{book.summary}</p>
+
+            <div>
+                {/* reviews here */}
+            </div>
         </>
        
     )
 }
 
 export default BookPage
+
