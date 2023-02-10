cba8dd23d232719cb660948640a1aea743585d38
Maximilian Alba <albam476@gmail.com>
2023-02-09 21:48:23 -0500

    add formatting conditionals

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -21,11 +21,11 @@ function ReviewCard({review , givenUser}){
        <>
        {user ?  
        <div>
-        {book ? <h1>{book.title}</h1> : <></>}
+        {givenUser && book ? <h1>{book.title}</h1> : <></>}
         <h1>{review.title}</h1>
         <p>{review.rating}</p>
         <p>{review.content}</p>
-        <p>{user.name}</p>
+        {!givenUser ? <p>{user.name}</p> : <></>}
         {!givenUser ? <img src={user.avatar_url} alt="user avatar"/> : <></>}
        </div> : <p>loading review</p>}
        </>
