dd49b977f2490caf7cdb82ce135e68a89d17a247
Maximilian Alba <albam476@gmail.com>
2023-02-09 21:37:47 -0500

    remove comments and render reviews

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -3,33 +3,27 @@ import ReviewCard from "./ReviewCard";
 
 function ProfilePage({user}){
 
-    const [profile, setProfile] = useState({})
     const [reviews, setReviews] = useState([])
 
-    // useEffect(() => {
-    //     fetch(`http://localhost:9292/users/${user.id}`)
-    //         .then(r => r.json())
-    //         .then(data => setProfile(data))
-    // }, [])
     
-    // useEffect(() => {
-    //     fetch(`http://localhost:9292/reviews/users/${user.id}`)
-    //         .then(r => r.json())
-    //         .then(data => setReviews(data))
-    // }, [])    
+    useEffect(() => {
+        fetch(`http://localhost:9292/users/${user.id}/reviews`)
+            .then(r => r.json())
+            .then(data => setReviews(data))
+    }, [])    
 
-    // function renderReviews (reivewsArr) {
-    //     const renderedReviews = reivewsArr.map((review) => <ReviewCard review={review}/>)
-    //     return renderedReviews
-    // }
+    function renderReviews (reivewsArr) {
+        const renderedReviews = reivewsArr.map((review) => <ReviewCard review={review} givenUser={true}/>)
+        return renderedReviews
+    }
 
     return(
         <>
-            {/* <h1>{user.name}</h1>
+            <h1>{user.name}</h1>
             <img src={user.avatar_url} alt="user avatar"/>
-            <p>{user.bio}</p> */}
+            <p>{user.bio}</p>
             <h1>PROFILE PAGE HERE</h1>
-            {/* {renderReviews(reviews)} */}
+            {renderReviews(reviews)}
         </>
 
     )
