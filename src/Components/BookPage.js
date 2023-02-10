e429c64bb1658106cc3381a452432ab3c5985d80
Maximilian Alba <albam476@gmail.com>
2023-02-09 20:43:04 -0500

    Build bookpage with fetches to reviews and user

--- src/Components/BookPage.js
+++ src/Components/BookPage.js
@@ -2,17 +2,27 @@ import {useState, useEffect} from "react";
 import ReviewCard from './ReviewCard'
 import { useParams } from "react-router-dom";
 
-function BookPage({book}){
+function BookPage(){
 
+    const {id} = useParams()
+    const [book, setBook] = useState(undefined)
     const [reviews, setReviews] = useState([])
     const [renderedReviews, setRenderedReviews] = useState(null)
-    const {id} = useParams()
+    
     useEffect(() => {
-        fetch(`http:localhost:9292/books/${id}/reviews`)
+        fetch(`http://localhost:9292/books/${id}/reviews`)
         .then(r => r.json())
         .then(data => setReviews(data))
     }, [])
    
+
+    useEffect(() => {
+        fetch(`http://localhost:9292/books/${id}`)
+        .then(r => r.json())
+        .then(data => setBook(data))
+    }, [])
+   
+
     useEffect(() => {
         setRenderedReviews(createReviewsList(reviews))
     }, [reviews])
@@ -22,17 +32,21 @@ function BookPage({book}){
         return reviewList
     }
 
+    
     return (
         <>
-            <h1>{book.title}</h1>
+            {book ? <> <h1>{book.title}</h1>
             <img src={book.cover_url} alt="book cover image"></img>
             <p>By: {book.author}</p>
             <p>{book.subgenre}</p>
             <p>{book.summary}</p>
+            <p>Rating is {book.rating}</p>
 
             <div>
                 {renderedReviews ? renderedReviews : <p>No reviews have been made</p>}
             </div>
+            
+            </> : <p>Loading Book</p>}
         </>
        
     )
