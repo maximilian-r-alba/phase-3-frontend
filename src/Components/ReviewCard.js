4cd09f8a7afe9ee9d0b2ab8082804cb58fcf1e0c
Maximilian Alba <albam476@gmail.com>
2023-02-09 20:41:08 -0500

    Add fetch for reviews and user in reviewCard

--- src/Components/ReviewCard.js
+++ src/Components/ReviewCard.js
@@ -1,14 +1,24 @@
-import { useEffect } from "react"
+import { useEffect , useState } from "react"
 function ReviewCard({review}){
-
+    const [user, setUser] = useState(undefined)
+    useEffect(() => {
+        fetch(`http://localhost:9292/users/${review.user_id}`)
+        .then(r => r.json())
+        .then(data => setUser(data))
+    },[])
 
     return(
+       <>
+       {user ?  
        <div>
         <h1>{review.title}</h1>
         <p>{review.rating}</p>
         <p>{review.content}</p>
-        <p>{review.user.name}</p>
-       </div>
+        <p>{user.name}</p>
+        <img src={user.avatar_url} alt="user avatar"/>
+       </div> : <p>loading review</p>}
+       </>
+      
     )
 }
 
