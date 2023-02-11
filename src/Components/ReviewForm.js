20d988581929081d0ebd91fdaf5123315227c2a3
Maximilian Alba <albam476@gmail.com>
2023-02-11 16:57:41 -0500

    Make form functional

--- src/Components/ReviewForm.js
+++ src/Components/ReviewForm.js
@@ -1,26 +1,53 @@
 import  styled  from "styled-components"
 import { useState } from "react"
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
 
-function ReviewForm({ user , book_id , handleViewForm}){
+function ReviewForm({ book_id , handleViewForm}){
 
-    const [reviewValues, setReviewValues] = useState()
+    const user = useContext(UserContext)
+    const [reviewValues, setReviewValues] = useState({user_id: user.id, book_id: book_id, title: "", rating: "", content: ""})
 
-    console.log(book_id)
+    function updateFormValues(e){
+        
+        const key = e.target.name
+        if(key == "rating")
+        {
+            const value = parseFloat(e.target.value)
+            setReviewValues({...reviewValues, [key]: value})
+        }
+
+        else{
+            const value = e.target.value    
+            setReviewValues({...reviewValues, [key]: value})
+        }
+ 
+    
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        console.log(reviewValues)
+    }
+
     return (
         <StyledDiv>
             <button onClick={handleViewForm}>X</button>
-            <form>
+            <form onSubmit={handleSubmit}>
             <label>
                 Title:
-                <input type="text" name="title" />
+                <input type="text" name="title" value={reviewValues.title} onChange={updateFormValues}/>
             </label>
             <label>
                 Rating:
-                <input type="text" name="title" />
+                <input type="number" name="rating" min="1" max="5" value={reviewValues.rating} onChange={updateFormValues}/>
             </label>
             <label>
                 Reivew:
-                <textarea type="text" name="title" />
+                <textarea type="text" name="content" value={reviewValues.content} onChange={updateFormValues}/>
+            </label>
+            <label>
+                <input type="submit"/>
             </label>
             </form>
         </StyledDiv>
