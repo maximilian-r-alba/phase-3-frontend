395728172018bbc743aade6bb432d246f8bfad00
Maximilian Alba <albam476@gmail.com>
2023-02-18 13:40:51 -0500

    Add handler for changing reviews

--- src/Components/ProfilePage.js
+++ src/Components/ProfilePage.js
@@ -2,11 +2,14 @@ import {useState , useEffect} from "react";
 import ReviewCard from "./ReviewCard";
 import { useContext } from "react";
 import { UserContext } from "./UserContext";
-
-function ProfilePage(){
+import { createPortal } from "react-dom";
+import ReviewForm from "./ReviewForm";
+function ProfilePage({portalSite}){
 
     const [reviews, setReviews] = useState([])
     const user = useContext(UserContext)
+    const [viewReviewForm, setViewReviewForm] = useState(false)
+    const [passedReview, setPassedReview] = useState(undefined)
     
     useEffect(() => {
         fetch(`http://localhost:9292/users/${user.id}/reviews`)
@@ -14,9 +17,33 @@ function ProfilePage(){
             .then(data => setReviews(data))
     }, [])    
 
+    function handleReviewChanges(reviewValues , method){
+
+        const reviewID = reviewValues.id
+        const filteredReviews = reviews.filter((review) => review.id !== reviewID)
+        switch (method){
+
+            case 'patch':
+                const patchReviewArr = [reviewValues].concat(filteredReviews)
+                setReviews(patchReviewArr)
+            break;
+
+            case 'delete':
+                setReviews(filteredReviews)
+            break;
+        }
+    }
+
+
     function renderReviews (reivewsArr) {
-        const renderedReviews = reivewsArr.map((review) => <ReviewCard key={`reviewKey${review.id}`} review={review} givenUser={true}/>)
+        const renderedReviews = reivewsArr.map((review) => <ReviewCard key={`reviewKey${review.id}`} handleViewForm={handleViewForm} handleReviewChanges={handleReviewChanges} setPassedReview={setPassedReview} review={review} givenUser={true}/>)
         return renderedReviews
+    }
+
+
+    function handleViewForm(e){
+        setViewReviewForm(viewReviewForm => !viewReviewForm)
+        setPassedReview(undefined)
     }
 
     return(
@@ -27,6 +54,8 @@ function ProfilePage(){
             <ul>
             {renderReviews(reviews)}
             </ul>
+
+            {viewReviewForm ? createPortal(<ReviewForm review={passedReview} handleReviewChanges={handleReviewChanges} handleViewForm = {handleViewForm} />, portalSite) : <></>}
             
         </>
 
