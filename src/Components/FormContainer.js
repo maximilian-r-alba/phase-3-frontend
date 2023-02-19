e2861c1140bce6b387b6f8573488619500f87a08
Maximilian Alba <albam476@gmail.com>
2023-02-18 19:58:18 -0500

    Create form container

---
+++ src/Components/FormContainer.js
@@ -0,0 +1,24 @@
+import styled from "styled-components"
+
+function FormContainer ({viewForm , form}) {
+    return (
+        <>
+        <StyledDiv >
+            {viewForm ? form : <></>}
+        </StyledDiv>
+            
+        </>
+    )
+}
+
+export default FormContainer
+
+const StyledDiv = styled.div `
+position: fixed;
+top: 15vh;
+left: 35vw;
+border: solid;
+height: 70vh;
+width: 30vw;
+opacity: 1;
+background-color: white;`
