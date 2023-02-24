96306f907029e7b6aa69286279d3f2d748aada01
Maximilian Alba <albam476@gmail.com>
2023-02-23 19:53:53 -0500

    Add top ten books for landing page

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,15 +1,56 @@
 import { UserContext } from "./UserContext"
 import { useContext } from "react"
+import { useState , useEffect } from "react"
+import BooksCard from "./BooksCard"
+import styled from "styled-components"
 
-function LandingPage () {
+function LandingPage ({books , setBooks}) {
    const user = useContext(UserContext) 
+   const [cards, setCards] = useState(undefined)
+
+   useEffect(() => {
+    fetch(`http://localhost:9292/books/toprated`).then(r => r.json()).then(data => setBooks(data))
+   }, [])
+
+   useEffect(() => {
+    setCards(createCards(books))
+   }, [books])
+
+   function createCards (bookArr) {
+
+     return (bookArr.map((book) => <BooksCard key={book.id} book={book} />))
+   }
+
     return(
-        <>
-        {user ? <h1>WELCOME {user.name}</h1> : <h1>WELCOME LANDING PAGE</h1>}
-       </>
+        <LandingContainer>
+        {user ? <h1>WELCOME {user.name}</h1> : <h1>Welcome</h1>}
+        {cards ? 
+        <div>
+            <h1>Top Ten Books</h1>
+            <TopTenContainer>
+                {cards} 
+            </TopTenContainer>
+        </div>
+       
+        : <></>}
+       </LandingContainer>
     )
 
    
 }
 
 export default LandingPage
+
+const TopTenContainer = styled.div`
+display: flex;
+gap: 60px;
+width: 50vw;
+overflow: scroll;
+overflow-y: hidden;
+`
+const LandingContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+gap: 100px;
+`
